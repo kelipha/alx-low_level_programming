a1654f98e15ca532831c09b095e57d43c67b20b7
Kehinde Adinlewa <adinlewakehinde@gmail.com>
2022-09-15 14:16:20 -0700

    4-putsv1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
 *main	-	print the string in the put function
 *
 *Desription: using the main function
-*this program prints "Programming is like buildig a multillingual puzzle
+*this program prints "Programming is like building a multilingual puzzle"
 *Return: 0
 */
 int main(void)
